40afa21e36e6230a64abcefd88e6cda1fa7d57c6
dannielgloria <danniel.gloria@gmail.com>
2026-02-04 19:47:30 -0600

    feat: Se añade configuracion a Cloudinary y manejo de errores visuales

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     const uri = process.env.MONGODB_URI;
-    if (!uri) throw new Error('MONGODB_URI is not defined in environment variables');
+    if (!uri) throw new Error('❌ MONGODB_URI is not defined in environment variables');
 
     mongoose.set('strictQuery', true);
 
